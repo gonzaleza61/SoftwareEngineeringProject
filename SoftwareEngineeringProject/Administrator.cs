0ed42bf1ec158eb0408589568de154efb77105a2
Luinceanera <38448169+Luinceanera@users.noreply.github.com>
2019-03-23 20:24:15 -0500

    Add files via upload

---
+++ SoftwareEngineeringProject/Administrator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Administrator
+{
+    public string adminName;
+    public Administrator()
+    {
+
+    }
+    public Administrator(string theAdminName)
+    {
+        adminName = theAdminName;
+    }
+    public string getAdminName()
+    {
+        return adminName;
+    }
+    public void setAdminName(string theAdminName)
+    {
+        adminName = theAdminName;
+    }
+}
---
+++ SoftwareEngineeringProject/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Student
+{
+    public string studentName;
+    public int studentID;
+    public string registeredCourses;
+    public int examScores;
+    public double GPACalculation;
+    public Student()
+    {
+
+    }
+    public Student(string theStudentName, int theStudentID, string theRegisteredCourses, int theExamScores, double theGPACalculation)
+    {
+        studentName = theStudentName;
+        studentID = theStudentID;
+        registeredCourses = theRegisteredCourses;
+        examScores = theExamScores;
+        GPACalculation = theGPACalculation;
+    }
+    public string getStudentName()
+    {
+        return studentName;
+    }
+    public int getStudentID()
+    {
+        return studentID;
+    }
+    public string getRegisteredCourses()
+    {
+        return registeredCourses;
+    }
+    public int getExamScores()
+    {
+        return examScores;
+    }
+    public double getGPACalculation()
+    {
+        return GPACalculation;
+    }
+    public void setStudentName(string theStudentName)
+    {
+        studentName = theStudentName;
+    }
+    public void setStudentID(int theStudentID)
+    {
+        studentID = theStudentID;
+    }
+    public void setRegisteredCourses(string theRegisteredCourses)
+    {
+        registeredCourses = theRegisteredCourses;
+    }
+    public void setExamScores(int theExamScores)
+    {
+        examScores = theExamScores;
+    }
+    public void setGPACalculation(double theGPACalculation)
+    {
+        GPACalculation = theGPACalculation;
+    }
+}
