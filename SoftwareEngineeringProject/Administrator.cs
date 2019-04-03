beb69b09955855d209b738e250d6083e6c822e79
Luinceanera <38448169+Luinceanera@users.noreply.github.com>
2019-04-03 16:33:39 -0500

    Update Administrator.cs

--- SoftwareEngineeringProject/Administrator.cs
+++ SoftwareEngineeringProject/Administrator.cs
@@ -7,20 +7,41 @@ using System.Threading.Tasks;
 public class Administrator
 {
     public string adminName;
+    public int adminID;
+    private string adminPassword;
+    public bool account;
     public Administrator()
     {
 
     }
-    public Administrator(string theAdminName)
+    public Administrator(string theAdminName, int theAdminID, bool theAccount)
     {
         adminName = theAdminName;
+        adminID = theAdminID;
+        account = theAccount;
     }
     public string getAdminName()
     {
         return adminName;
     }
+    public int getAdminID()
+    {
+        return adminID;
+    }
+    public bool getAccount()
+    {
+        return account;
+    }
     public void setAdminName(string theAdminName)
     {
         adminName = theAdminName;
+    }
+    public void setAdminID(int theAdminID)
+    {
+        adminID = theAdminID;
+    }
+    public void setAccount(bool theAccount)
+    {
+        account = theAccount;
     }
 }
