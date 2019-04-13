dfb48c89deb53544fb1f9901e20678675b0b7845
Luinceanera <38448169+Luinceanera@users.noreply.github.com>
2019-04-13 18:56:06 -0500

    Create Student.cs

---
+++ LMS Prototype/LMS Prototype/Student.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Inheritence from Course */
+
+public class Student
+{
+    public string studentName;
+    public int studentID;
+    private string studentPassword;
+    public string registeredCourses;
+    public int examScores;
+    public double GPACalculation;
+    public bool account;
+    public Student()
+    {
+
+    }
+    public Student(string theStudentName, int theStudentID, string theStudentPassword, string theRegisteredCourses, int theExamScores, double theGPACalculation, bool theAccount)
+    {
+        studentName = theStudentName;
+        studentID = theStudentID;
+        studentPassword = theStudentPassword;
+        registeredCourses = theRegisteredCourses;
+        examScores = theExamScores;
+        GPACalculation = theGPACalculation;
+        account = theAccount;
+    }
+    public string getStudentName()
+    {
+        return studentName;
+    }
+    public int getStudentID()
+    {
+        return studentID;
+    }
+    public string getRegisteredCourses()
+    {
+        return registeredCourses;
+    }
+    public int getExamScores()
+    {
+        return examScores;
+    }
+    public double getGPACalculation()
+    {
+        return GPACalculation;
+    }
+    public bool getAccount()
+    {
+        return account;
+    }
+    public void setStudentName(string theStudentName)
+    {
+        studentName = theStudentName;
+    }
+    public void setStudentID(int theStudentID)
+    {
+        studentID = theStudentID;
+    }
+    public void setRegisteredCourses(string theRegisteredCourses)
+    {
+        registeredCourses = theRegisteredCourses;
+    }
+    public void setExamScores(int theExamScores)
+    {
+        examScores = theExamScores;
+    }
+    public void setGPACalculation(double theGPACalculation)
+    {
+        GPACalculation = theGPACalculation;
+    }
+    public void setAccount(bool theAccount)
+    {
+        account = theAccount;
+    }
+    //public void account()
+    //{
+
+    //}
+    public void logIn()
+    {
+
+    }
+    public void logOff()
+    {
+
+    }
+    public void viewRecord()
+    {
+
+    }
+}
