4ce15c51a1737a80abd44aa70cd762a75fe0f517
Luinceanera <38448169+Luinceanera@users.noreply.github.com>
2019-04-13 18:58:06 -0500

    Create Administrator.cs

---
+++ LMS Prototype/LMS Prototype/Administrator.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Inheritance from Student and Course */
+
+public class Administrator
+{
+    public string adminName;
+    public int adminID;
+    private string adminPassword;
+    public bool account;
+    public Administrator()
+    {
+
+    }
+    public Administrator(string theAdminName, int theAdminID, bool theAccount)
+    {
+        adminName = theAdminName;
+        adminID = theAdminID;
+        account = theAccount;
+    }
+    public string getAdminName()
+    {
+        return adminName;
+    }
+    public int getAdminID()
+    {
+        return adminID;
+    }
+    public bool getAccount()
+    {
+        return account;
+    }
+    public void setAdminName(string theAdminName)
+    {
+        adminName = theAdminName;
+    }
+    public void setAdminID(int theAdminID)
+    {
+        adminID = theAdminID;
+    }
+    public void setAccount(bool theAccount)
+    {
+        account = theAccount;
+    }
+    public void logIn()
+    {
+        
+    }
+    public void logOff()
+    {
+        
+    }
+    public void addRecord()
+    {
+        
+    }
+    public void deleteRecord()
+    {
+        
+    }
+    public void insertRecord()
+    {
+        
+    }
+    public void modifyRecord()
+    {
+        
+    }
+    public void browseRecord()
+    {
+        
+    }
+    public void checkGPA()
+    {
+        
+    }
+}
