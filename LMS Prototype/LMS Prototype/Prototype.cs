ac8b97d12c4c61bf862bc24bf0bb6da2b699a083
chrisfel2018 <44661529+chrisfel2018@users.noreply.github.com>
2019-04-24 08:32:15 -0500

    Added student login (for one person only: bob)
    1.  Student login only works for one student, bob (user name = bob, password = 1234).
    2.  Upon successful student login, only that student's record can be seen (only for student: bob.
    3.  Buttons (add, delete, insert) have been disabled.  When student clicks on one of these, "Access Denied" message is shown.

--- LMS Prototype/LMS Prototype/Prototype.cs
+++ LMS Prototype/LMS Prototype/Prototype.cs
@@ -18,6 +18,7 @@ namespace LMS_Prototype
         DataTable table = new DataTable();    
 
         public static bool isAdmin = false;
+        public static bool isStudent = false;
 
         //Create student object
         Student bob = new Student("Bob", 1, "bob", 3.99, true);
@@ -31,33 +32,48 @@ namespace LMS_Prototype
         public Prototype()
         {
             
-            string adminName = "";
+            string name = "";
             string password = "";
+            bool isNameFound = false;
 
-            while (!(adminName.Equals("admin")))
+            //Check if an administrator is logging in
+
+            //while (!(name.Equals("admin")) && !(name.Equals("bob")))
+
+            while (!isNameFound)
             {
-                adminName = Interaction.InputBox("Enter user name: ", "Login (user: admin, pass: 1234)", "", 500, 300);     //check code
+                name = Interaction.InputBox("Enter user name: ", "Login (user: admin or bob, pass: 1234)", "", 500, 300);     //check code
 
-                if (adminName.Equals("admin"))
-                    adminName = "admin";
+                if (name.Equals("admin"))
+                {
+                    isAdmin = true;
+                    name = "admin";
+                    isNameFound = true;
+                }
+                    
+                else if (name.Equals("bob"))
+                {
+                    isStudent = true;
+                    name = "bob";
+                    isNameFound = true;
+                }
+                    
                 else
                     MessageBox.Show("User not found. Try again.");
             }
 
             while (!(password.Equals("1234")))
             {
-                password = Interaction.InputBox("Enter password: ", "Password (user: admin, pass: 1234)", "", 500, 300);    //check code
+                password = Interaction.InputBox("Enter password: ", "Password (user: admin or bob, pass: 1234)", "", 500, 300);    //check code
 
                 if (password.Equals("1234"))
                     password = "1234";
                 else
                     MessageBox.Show("Password not found. Try again.");
             }
+
             
-
-
-
-
+            
 
 
             InitializeComponent();
@@ -95,17 +111,26 @@ namespace LMS_Prototype
             //Output sample of studentA using Student methods onto GUI
             //table.Rows.Add(bob.getID(), "Bob", bob.getGPA(), bob.getCourse1().getExam1(), bob.getCourse1().getExam2(), bob.getCourse1().getExam3(), 100, 100, 100, 100, 100, 99);
 
-            table.Rows.Add(1, "Bob", 3.75, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 99);
-            table.Rows.Add(2, "Cob", 3.50, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 88);
-            table.Rows.Add(3, "Dob", 3.00, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 77);
-            table.Rows.Add(4, "Fob", 2.50, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 66);
-            table.Rows.Add(5, "Gob", 2.00, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 55);
-            table.Rows.Add(6, "Hob", 1.50, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 44);
-            table.Rows.Add(7, "Job", 1.25, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 33);
-            table.Rows.Add(8, "Kob", 1.00, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 22);
-            table.Rows.Add(9, "Lob", 0.10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11);
-            table.Rows.Add(10, "Mob", 0.05, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
-            table.Rows.Add(11, "Nob", 0.00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            if(isStudent)
+            {
+                table.Rows.Add(1, "Bob", 3.75, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 99);
+            }
+
+            else if(isAdmin)
+            {
+                table.Rows.Add(1, "Bob", 3.75, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 99);
+                table.Rows.Add(2, "Cob", 3.50, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 88);
+                table.Rows.Add(3, "Dob", 3.00, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 77);
+                table.Rows.Add(4, "Fob", 2.50, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 66);
+                table.Rows.Add(5, "Gob", 2.00, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 55);
+                table.Rows.Add(6, "Hob", 1.50, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 44);
+                table.Rows.Add(7, "Job", 1.25, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 33);
+                table.Rows.Add(8, "Kob", 1.00, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 22);
+                table.Rows.Add(9, "Lob", 0.10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11);
+                table.Rows.Add(10, "Mob", 0.05, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5);
+                table.Rows.Add(11, "Nob", 0.00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            }
+            
 
             dataGridView1.DataSource = table;
         }
@@ -127,26 +152,33 @@ namespace LMS_Prototype
 
         private void insert(object sender, EventArgs e)
         {
-            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            if(isStudent)
+            {
+                MessageBox.Show("Access Denied.  You must be an Administrator to insert a record.");
+            }
 
-            int ID = 0;
-            string name = "";
-            double GPA = 3.5;
+            else if(isAdmin)
+            {
+                int rowIndex = dataGridView1.CurrentCell.RowIndex;
 
-            ID = int.Parse(Interaction.InputBox("Enter ID: ", "Row Input", "", 500, 300));
+                int ID = 0;
+                //string name = "";
+                //double GPA = 3.5;
 
-            //name = Interaction.InputBox("Enter name: ", "Row Input", "", 500, 300);
+                ID = int.Parse(Interaction.InputBox("Enter ID: ", "Row Input", "", 500, 300));
 
-            //GPA = double.Parse(Interaction.InputBox("Enter GPA: ", "Row Input", "", 500, 300));
+                //name = Interaction.InputBox("Enter name: ", "Row Input", "", 500, 300);
 
-            
+                //GPA = double.Parse(Interaction.InputBox("Enter GPA: ", "Row Input", "", 500, 300));
 
-            DataRow dr = table.NewRow();
-            dr[0] = ID;
-            dr[1] = "Enter Info";
-            //dr[2] = GPA;
-            table.Rows.InsertAt(dr, rowIndex);
 
+
+                DataRow dr = table.NewRow();
+                dr[0] = ID;
+                dr[1] = "Enter Info";
+                //dr[2] = GPA;
+                table.Rows.InsertAt(dr, rowIndex);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -160,15 +192,31 @@ namespace LMS_Prototype
         {
             //ask & verify ID, name, GPA, exams...
 
-            table.Rows.Add(0, "Enter Info", 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            if (isStudent)
+            {
+                MessageBox.Show("Access Denied.  You must be an Administrator to add a record.");
+            }
+
+            else if(isAdmin)
+            {
+                table.Rows.Add(0, "Enter Info", 0.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            }
         }
 
         private void delete(object sender, EventArgs e)
         //Purpose: delete selected row
         {
-            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            if (isStudent)
+            {
+                MessageBox.Show("Access Denied.  You must be an Administrator to delete a record.");
+            }
 
-            table.Rows.RemoveAt(rowIndex);
+            else if(isAdmin)
+            {
+                int rowIndex = dataGridView1.CurrentCell.RowIndex;
+
+                table.Rows.RemoveAt(rowIndex);
+            }
         }
 
         private void logOff(object sender, EventArgs e)
